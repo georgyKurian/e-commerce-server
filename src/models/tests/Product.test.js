e38d3374e758f13ba9e68428674a2cc0dfb2a07b
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-09-25 09:20:08 -0400

    Removed mocha and added jest

---
+++ src/models/tests/Product.test.js
@@ -0,0 +1,7 @@
+import ProductModel from '../Product';
+
+test('It accepts all params and implements all methods', () => {
+  const raw = { id: "1234", name: "WHMIS", price: 23, images: ["1"] };
+  const product = new ProductModel(raw);
+  expect(product.getData()).toEqual(raw);
+});
