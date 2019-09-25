906cb4bbf85a3e0ddaa43f9af53a42dc4d9abe74
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-09-25 17:01:49 -0400

    Added seed.js

---
+++ server/scripts/seed.js
@@ -0,0 +1,30 @@
+import mongoose, { schema, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  email: String,
+  username: String,
+  role: String
+});
+const UserModel = mongoose.model("User", UserSchema);
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on("error", error => {
+  console.error(error);
+});
+
+db.on("open", () => {
+  console.log("Database connection is open!");
+  const user = new UserModel({
+    email: "jon1@test.ca",
+    username: "jon2@test.ca",
+    role: "customer"
+  });
+  user.save(error => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("User saved");
+    }
+  });
+});
