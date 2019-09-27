3da794d4161153d7da64b4035027513b5fd67b99
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-09-26 22:29:49 -0400

    CHanged to cloud DB

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -6,7 +6,8 @@ const UserSchema = new Schema({
   role: String
 });
 const UserModel = mongoose.model("User", UserSchema);
-mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://ecommerceapp:Q35JgRaiYKe5@e-commerce-1n6e9.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on("error", error => {
