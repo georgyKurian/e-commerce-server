147d2ec04a8b05935a8c6e218387e41bdf75d65a
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-09-30 17:01:01 -0400

    Sperated rotes to different file

---
+++ server/routes/users.js
@@ -0,0 +1,40 @@
+import { UserModel } from "../models/User";
+
+export default app => {
+  app.get("/", (req, res) => {
+    res.send({ dateTime: new Date().toJSON() });
+  });
+
+  app.get("/v1/users", async (req, res) => {
+    const users = (await UserModel.find()) || [];
+    res.send(users);
+  });
+
+  app.get("/v1/users/:id", async (req, res) => {
+    try {
+      const user = await UserModel.findById(req.params.id);
+      console.log(user);
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(400).end();
+      }
+    } catch (e) {
+      res.status(404).end();
+    }
+  });
+
+  app.post("/v1/users", (req, res) => {
+    console.log(req.body);
+    res.status(200).end();
+  });
+
+  app.put("/v1/users/:id", async (req, res) => {
+    res.status(200).end();
+  });
+
+  app.delete("/v1/users/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send(users);
+  });
+};
