84821933a1cc0d1f7abd70c07dc44522aa20d144
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-09-30 12:19:44 -0400

    Added Create, Update & Delete API functionalities to products

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,14 @@ import mongoose from "mongoose";
 // mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(
   "mongodb+srv://ecommerceapp:Q35JgRaiYKe5@e-commerce-1n6e9.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
 );
+
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
--- server/index.js
+++ server/index.js
@@ -54,7 +54,6 @@ app.delete("/v1/users/:id", (req, res) => {
   res.send(users);
 });
 
-
 app.get("/v1/products", async (req, res) => {
   const products = (await ProductModel.find()) || [];
   res.send(products);
@@ -73,18 +72,54 @@ app.get("/v1/products/:id", async (req, res) => {
   }
 });
 
-app.post("/v1/products", (req, res) => {
-  console.log(req.body);
-  res.send(users);
+app.post("/v1/products", async (req, res) => {
+  try {
+    const product = await ProductModel.create(req.body);
+    if (product) {
+      res.send(product).end();
+    } else {
+      res.status(400).end();
+    }
+  } catch (e) {
+    res.status(404).end();
+  }
 });
 
 app.put("/v1/products/:id", async (req, res) => {
-  res.statusCode(400);
+  try {
+    await ProductModel.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true },
+      function(err, updatedProduct) {
+        if (err) {
+          console.log(err);
+          res.status(400).end();
+        } else {
+          res.send(updatedProduct).end();
+        }
+      }
+    );
+  } catch (e) {
+    res.status(404).end();
+  }
 });
 
-app.delete("/v1/products/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(users);
+app.delete("/v1/products/:id", async (req, res) => {
+  try {
+    await ProductModel.findOneAndDelete({ _id: req.params.id }, function(
+      err,
+      updatedProduct
+    ) {
+      if (err) {
+        res.status(400).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  } catch (e) {
+    res.status(404).end();
+  }
 });
 
 // catch 404 and forward to error handler
@@ -103,6 +138,9 @@ app.use(function(err, req, res, next) {
   res.json({ error: err });
 });
 
+/**
+ * Listening to port
+ */
 app.listen(port, () => {
   console.log(`App listening on port ${port}! http://localhost:${port}`);
 });
