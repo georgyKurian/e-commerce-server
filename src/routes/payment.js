c2f31ad11ecc9a77b1d9829b8e521c66fc254143
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-05-15 14:47:48 -0400

    Added web kooh for stripe

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -19,4 +19,32 @@ export default (app) => {
       res.status(400).end();
     }
   });
+
+  app.post('/v1/stripe/webhook', async (req, res) => {
+    try {
+      const { type, data } = req.body;
+
+      // Handle the event
+      switch (type) {
+        case 'payment_intent.succeeded':
+          const paymentIntent = data.object;
+          console.log('PaymentIntent was successful!');
+          break;
+        case 'payment_method.attached':
+          const paymentMethod = data.object;
+          console.log('PaymentMethod was attached to a Customer!');
+          break;
+          // ... handle other event types
+        default:
+          // Unexpected event type
+          return res.status(400).end();
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      res.json({ received: true });
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
+  });
 };
