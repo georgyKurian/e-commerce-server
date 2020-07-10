ad7130fbbe84827874ef5a764bc6c1862f753195
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-07-10 16:26:22 -0400

    WIP - adding product data from adidas

---
+++ data/products.js
@@ -0,0 +1,207 @@
+[
+  {
+    "name": "PREDATOR MUTATOR 20.1 FIRM GROUND CLEATS",
+    "slug": "copa-20.4-firm-ground-cleats",
+    "code": "EE9555",
+    "price": 30000,
+    "tags": {
+      "gender" : ["men"],
+      "color": ["black","pink"],
+      "use": ["sports","soccer","training"],
+      "type":["shoe"],
+      "other":["new","featured"]
+    },
+    "description" :{
+      "subtitle":"MID-CUT CLEATS FOR TAKING CHARGE ON FIRM GROUND.",
+      "content":"Your confidence consumes them. Your vision leaves them with nowhere to hide. Seize your unfair advantage and take control in these adidas Predator Mutator 20.1 soccer cleats. You'll boss every game in a knit textile upper that adapts to your foot's shape. Rubber spines on the forefoot grip the ball, letting you add whip to free kicks and cross-field passes. The split outsole digs in for domination on firm ground."
+    },
+    "specification" :[
+      "Regular fit",
+      "Lace closure",
+      "Coated textile upper",
+      "Mid-cut firm ground soccer cleats",
+      "Micro-textured upper for improved ball control",
+      "Demonscale 3D-print strike zone",
+      "Product colour: Core Black / Cloud White / Active Red",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "Copa 20.4 Firm Ground Cleats",
+    "slug": "copa-20.4-firm-ground-cleats",
+    "code": "G28527",
+    "price": 6500,
+    "tags": {
+      "gender" :["men"],
+      "color": ["black","grey"],
+      "use": ["sports","soccer","professional"],
+      "type":["shoe"],
+      "other":[]
+    },
+    "description" :{
+      "subtitle":"Leather cleats for an enhanced touch on firm ground.",
+      "content":"Play harder, win bigger. Always demand more. Your touch. Your team. In these soccer cleats you'll transform them both. Stitching on their durable synthetic forefoot stops the ball slipping off your foot. The outsole digs into firm ground to help you show your class. Step it up in adidas Copa 20.4 Firm Ground Cleats."
+    },
+    "specification" :[    
+      "Regular fit",
+      "Lace closure",
+      "Lightweight, durable synthetic leather upper",
+      "Firm ground soccer cleats",
+      "Stitched vamp for control",
+      "Product colour: Core Black / Core Black / Dgh Solid Grey  ",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "Nemeziz 19.1 Firm Ground Cleats",
+    "slug": "nemeziz-19.1-firm-ground-cleats",
+    "code": "EH0830",
+    "price": 30000,
+    "tags": {
+      "gender" :["men"],
+      "color": ["black","orange"],
+      "use": ["sports","soccer","training"],
+      "type":["shoe"],
+      "other":["new"]
+    },
+    "description" :{
+      "subtitle":"Adaptive cleats for agility on firm ground.",
+      "content":"Dodge expectations. Zig when they think you'll zag. Pass when they expect you to move. Unlock agility and power your unpredictability in adidas Nemeziz. These firm ground soccer cleats have a stretchy upper that offers quick access. Once you're in, it'll lock you down without limiting your game. The split outsole flexes with every jink and turn. The studs are arranged to help you run rings around your rivals."
+    },
+    "specification" :[
+      "Regular fit",
+      "Lace closure",
+      "Engineered textile upper",
+      "Tension Tape for a personalized fit",
+      "Firm ground soccer cleats",
+      "Flexible Torsion System",
+      "Product colour: Core Black / Core Black / Signal Orange",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "Mundial Team Shoes",
+    "slug": "mundial-team-shoes",
+    "code": "019228",
+    "price": 16000,
+    "tags": {
+      "gender" :["men"],
+      "color": ["black","white"],
+      "use": ["sports","soccer","indoor"],
+      "type":["shoe"],
+      "other":[]
+    },
+    "description" :{
+      "subtitle":"Legendary shoes with a touch of class.",
+      "content":"Crafted to deliver a world-class fit and touch, the Mundial continues its domination of courts across the globe. The suede and leather upper on these soccer shoes provides step-in comfort and assured control. A rubber outsole improves stability on turf and hard ground, while a foam midsole cushions every step."
+    },
+    "specification" :[
+      "Lace closure",
+      "Suede and leather upper",
+      "Soft synthetic lining; Traxion outsole",
+      "Die-cut EVA insole; Molded EVA midsole",
+      "Product colour: Black / Cloud White / Red",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "Terrex Eastrail Hiking Shoes",
+    "slug": "terrex-eastrail-hiking-shoes",
+    "code": "FV6860",
+    "price": 11000,
+    "tags": {
+      "gender" :["men","women"],
+      "color": ["aqua","black","orange"],
+      "use": ["hiking"],
+      "type":["shoe"],
+      "other":["clearance"]
+    },
+    "description" :{
+      "subtitle":"Hiking shoes with grip and support to do it all.",
+      "content":"Lace up and choose your destination. Urban walk or forest trail, the adidas Terrex Eastrail Hiking Shoes support day hikes and weekend adventures alike. Stable, secure footing gives you the confidence to explore on and off the trail."
+    },
+    "specification" :[
+      "Regular fit",
+      "Lace closure",
+      "Mesh and synthetic upper",
+      "Durable, lightweight cushioning",
+      "Multi-function hiking shoes",
+      "EVA midsole",
+      "Weight: 10.75 ounces (size 9)",
+      "Molded sockliner",
+      "Product colour: Hi-Res Aqua / Core Black / Signal Orange",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "Terrex Two Ultra Parley Trail Running Shoes",
+    "slug": "terrex-two-ultra-parley-trail-running-shoes",
+    "code": "FW9872",
+    "price": 25000,
+    "tags": {
+      "gender" :["men","women"],
+      "color": ["pink","black","orange"],
+      "use": ["running","training"],
+      "type":["shoe"],
+      "other":["hot"]
+    },
+    "description" :{
+      "subtitle":"Take on more trail in comfort, no matter the distance. Featuring full-length Boost cushioning and a Continental™ Rubber outsole for serious grip, it's time to hit the trail.",
+      "content":"Explore more in the new TERREX TWO Ultra Parley – designed to keep you comfortable on the trail, no matter the distance. The flexible upper features Parley Ocean Plastic – up-cycled plastic waste, intercepted on remote islands, beaches, coastal communities, and shorelines, preventing it from polluting our oceans. And with full-length Boost cushioning and a Continental™ Rubber outsole for serious grip, it's time to hit the trail."
+    },
+    "specification" :[
+      "Regular fit",
+      "Lace closure",
+      "Abrasion-resistant synthetic upper",
+      "Lightweight EVA cushioning",
+      "Cushioned trail running shoes",
+      "Boost midsole",
+      "Continental™ Rubber outsole",
+      "Product colour: Signal Pink / Core Black / Light Flash Orange",
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "",
+    "slug": "",
+    "code": "",
+    "price": 0,
+    "tags": {
+      "gender" :["men"],
+      "color": ["black","pink"],
+      "use": ["sports","soccer","training"],
+      "type":["shoe"],
+      "other":["new","featured"]
+    },
+    "description" :{
+      "subtitle":"",
+      "content":""
+    },
+    "specification" :[
+      
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  {
+    "name": "",
+    "slug": "",
+    "code": "",
+    "price": 0,
+    "tags": {
+      "gender" :["men"],
+      "color": ["black","pink"],
+      "use": ["sports","soccer","training"],
+      "type":["shoe"],
+      "other":["new","featured"]
+    },
+    "description" :{
+      "subtitle":"",
+      "content":""
+    },
+    "specification" :[
+      
+    ],
+    "sizes":[4,5,6,7,8,9,10,11,12,13]
+  },
+  
+]
