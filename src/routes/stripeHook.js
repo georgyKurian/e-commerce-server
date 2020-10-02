8a9ef4098fdc68131ccd49f0c328c003118fa5ad
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-10-01 20:27:29 -0400

    Fixed stripe webhook problem

--- src/routes/stripeHook.js
+++ src/routes/stripeHook.js
@@ -1,61 +1,67 @@
 import { OrderModel, ORDER_STATUS } from '../models/Order';
 
-const stripe = require('stripe')('sk_test_K8NejsGbnfInosovpfHLadFV001xXflaVN');;
+const stripe = require('stripe')('sk_test_K8NejsGbnfInosovpfHLadFV001xXflaVN');
 const bodyParser = require('body-parser');
 
 const endpointSecret = 'whsec_l1CaqgoRdUzO75LxNtI4ED7tvR3JOnQD';
-const paymentIntentRegexp = new RegExp(/\bpayment_intent.([a-z])*$/,'');
-
-function updateOrder(){
-
-}
+const paymentIntentRegexp = new RegExp(/\bpayment_intent.([a-z])*$/, '');
 
 export default (router) => {
-  router.post('', bodyParser.raw({type: 'application/json'}), async (req, res) => {
-    try {
-      // jovial-serene-zest-cozy
-      const signature = req.headers['stripe-signature'];
-      
-      let event;
-      let paymentIntent;          
-
+  router.post(
+    '',
+    bodyParser.raw({ type: 'application/json' }),
+    async (req, res) => {
       try {
-        event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
-      }
-      catch (err) {
-        res.status(400).send(`Webhook Error: ${err.message}`);
-      }
+        // jovial-serene-zest-cozy
+        const signature = req.headers['stripe-signature'];
 
-      if( paymentIntentRegexp.test(event.type) ){
-        paymentIntent = event.data.object;
-      }
+        let event;
+        let paymentIntent;
 
-      const order = OrderModel
-        .find({ paymentIntentId: paymentIntent.id,  })
-        .then((order)=>{
-          console.log(order);              
-        });
+        try {
+          event = stripe.webhooks.constructEvent(
+            req.body,
+            signature,
+            endpointSecret,
+          );
+        } catch (err) {
+          res.status(400).send(`Webhook Error: ${err.message}`);
+        }
 
-      switch (event.type) {
-        case 'payment_intent.succeeded':
-          order.status = ORDER_STATUS.PROCESSING;          
-        case 'payment_intent.amount_capturable_updated':
-        case 'payment_intent.payment_failed':
-        case 'payment_intent.processing':
-        case 'payment_intent.requires_action':          
-          console.log('PaymentIntent was successful!');
-          console.log(paymentIntent);
-          break;
-        default:
-          // Unexpected event type
+        if (paymentIntentRegexp.test(event.type)) {
+          paymentIntent = event.data.object;
+        } else {
           return res.status(400).end();
-      }
+        }
 
-      // Return a 200 response to acknowledge receipt of the event
-      res.json({ received: true });
-    } catch (e) {
-      console.error(e);
-      res.status(400).end();
-    }
-  });
+        OrderModel.findOne({ paymentIntentId: paymentIntent.id }).then((order) => {
+          if (order) {
+            switch (event.type) {
+              case 'payment_intent.amount_capturable_updated':
+                break;
+              case 'payment_intent.processing':
+                order.status = ORDER_STATUS.PROCESSING;
+                break;
+              case 'payment_intent.requires_action':
+                break;
+              case 'payment_intent.payment_failed':
+                order.status = ORDER_STATUS.FAILED;
+                break;
+              case 'payment_intent.succeeded':
+                order.status = ORDER_STATUS.COMPLETED;
+                break;
+              default:
+                // Unexpected event type
+                return res.status(400).end();
+            }
+            return order.save().then(() => res.json({ received: true }));
+          }
+          return res.status(400).end('Order not found!');
+        });
+      } catch (e) {
+        console.error(e);
+        res.status(400).end('Exception ocuured!');
+      }
+    },
+  );
 };
